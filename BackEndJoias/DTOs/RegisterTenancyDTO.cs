07fe2d4d2dd637467c030f39507a6870cd70e639
ThDarkCipher <ianbatista279@gmail.com>
2026-02-20 20:18:03 -0300

    Criando RegisterTenancyDTO e inserindo register-tenancy em UsersController

---
+++ BackEndJoias/DTOs/RegisterTenancyDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace GemaGestor.DTOs
+{
+    public class RegisterTenancyDTO
+    {
+        public string TenancyName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty; 
+        public string Password { get; set; } = string.Empty;
+    }
+}
